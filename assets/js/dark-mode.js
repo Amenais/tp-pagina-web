513df3f3e93b772c13eaba93bcee848a8872c219
Andres <mhaicoldramirez@gmail.com>
2023-05-27 21:23:07 -0300

    A

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,5 +1,9 @@
-const botonModoOscuro = document.querySelector('#modo-oscuro');
+// Obtén una referencia al botón y a la barra de navegación
+const toggleButton = document.getElementById('toggleButton');
+const navbar = document.getElementById('navbar');
 
-botonModoOscuro.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
+// Agrega un controlador de eventos al botón
+toggleButton.addEventListener('click', function() {
+  // Alternar la clase "show" en la barra de navegación para mostrar u ocultar
+  navbar.classList.toggle('show');
 });
