b9d07f34f74a443f5e3b950032c8ecb20cf60abf
Giuliano <thundergiu@hotmail.com>
2023-05-29 23:52:58 -0300

    Implementación de sugerencias desde la API

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,45 +1,19 @@
-const inputElement = document.getElementById('university-input');
-const datalist = document.getElementById('suggestions');
+fetch('../assets/json/universities.json')
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data); // Depuración: muestra los datos del archivo JSON en la consola
+    const universityInput = document.getElementById('university-input');
+    const suggestionsDatalist = document.getElementById('suggestions');
 
-window.onload = function () {
-  inputElement.addEventListener("mouseenter", function () {
-    fetchData(inputElement);
-  });
-}
-
-function fetchData(input) {
-  // Obtenemos el valor del input
-  const searchTerm = input.value;
-
-  // Obtenemos los datos del JSON
-  fetch('../assets/json/universities.json')
-    .then(response => response.json())
-    .then(data => {
-      // Filtramos los datos para obtener los que coinciden con el término de búsqueda
-      const filteredData = Object.values(data).filter(university => {
-        return university.name.toLowerCase().includes(searchTerm.toLowerCase());
-      });
-
-      // Creamos la lista desplegable con los resultados
-      const dropdown = document.createElement('div');
-      dropdown.classList.add('dropdown');
-
-      filteredData.forEach(university => {
-        const option = document.createElement('div');
-        option.classList.add('dropdown-option');
-        option.textContent = `${university.name}, ${university.country}`;
-        dropdown.appendChild(option);
-      });
-
-      // Vinculamos el datalist al input y mostramos la lista desplegable
-      input.setAttribute('list', 'suggestions');
-      datalist.innerHTML = '';
-      datalist.appendChild(dropdown);
-
-      // Eliminamos la lista desplegable si el valor del input está vacío
-      if (searchTerm === '') {
-        datalist.innerHTML = '';
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        const university = data[key];
+        const option = document.createElement('option');
+        option.value = university.name;
+        suggestionsDatalist.appendChild(option);
       }
-    })
-    .catch(error => console.error(error));
-}
+    }
+  })
+  .catch(error => {
+    console.error('Error al cargar el archivo JSON:', error);
+  });
